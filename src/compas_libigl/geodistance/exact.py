2ee2d4435001836af55ab77b021abadef34500cc
Tom Van Mele <vanmelet@ethz.ch>
2018-12-06 21:15:57 +0100

    returnin struct use key-index maps to make sure result is correct

--- src/compas_libigl/geodistance/exact.py
+++ src/compas_libigl/geodistance/exact.py
@@ -4,22 +4,30 @@ import compas
 import compas_libigl
 
 from compas.datastructures import Mesh
-# from compas.plotters import MeshPlotter
+from compas.plotters import MeshPlotter
+
+from compas.topology import mesh_quads_to_triangles
 
 import geodistance as g
 
 # mesh = Mesh.from_off('../../../data/libigl-tutorial-data/bunny.off')
-mesh = Mesh.from_obj('../../../data/libigl-tutorial-data/armadillo.obj')
+# mesh = Mesh.from_obj('../../../data/libigl-tutorial-data/armadillo.obj')
+
+mesh = Mesh.from_json(compas.get('tubemesh.json'))
+
+mesh_quads_to_triangles(mesh)
+
 
 key_index = mesh.key_index()
 
-V = numpy.array(mesh.get_vertices_attributes('xyz'))
-F = numpy.array([mesh.face_vertices(fkey) for fkey in mesh.faces()])
+V = numpy.array(mesh.get_vertices_attributes('xyz'), dtype=numpy.float64)
+F = numpy.array([[key_index[key] for key in mesh.face_vertices(fkey)] for fkey in mesh.faces()], dtype=numpy.int32)
 
 d = g.exact(V, F, 0)
 
 print(d)
 
-# plotter = MeshPlotter(mesh)
-# plotter.draw_faces()
-# plotter.show()
+plotter = MeshPlotter(mesh)
+plotter.draw_faces()
+plotter.draw_vertices(text={key: "{:.0f}".format(d[key_index[key]]) for key in mesh.vertices()})
+plotter.show()
