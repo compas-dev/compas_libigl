e59b2c228e0da1ba4aa89f01be191a7509dcf487
Ziqi Wang <qiqiustc@gmail.com>
2019-02-27 20:13:19 +0100

    add

--- examples/Example1/planar.py
+++ examples/Example1/planar.py
@@ -1,14 +1,19 @@
+import os
+import bpy
+import sys
+filepath = bpy.data.filepath
+directory = os.path.dirname(filepath)
+sys.path.append(directory)
+
 import numpy
 import compas
-
+import compas.datastructures
 from compas.datastructures import Mesh
-from compas.datastructures import mesh_flatness
 from compas.plotters import MeshPlotter
 from compas.utilities import i_to_rgb
-
 import planarize
 
-mesh1 = Mesh.from_json(compas.get('tubemesh.json'))
+mesh1 = Mesh.from_json('tubemesh.json')
 
 vertices, faces = mesh1.to_vertices_and_faces()
 
@@ -16,10 +21,10 @@ V1 = numpy.array(vertices, dtype=numpy.float64)
 F1 = numpy.array(faces, dtype=numpy.int32)
 V2 = planarize.planarize(V1, F1)
 
-mesh2 = Mesh.from_vertices_and_faces(V2, faces)
+#mesh2 = Mesh.from_vertices_and_faces(V2, faces)
 
-dev2 = mesh_flatness(mesh2, maxdev=0.02)
+#dev2 = mcompas.datastructures.esh_flatness(mesh2, maxdev=0.02)
 
-plotter = MeshPlotter(mesh2)
-plotter.draw_faces(facecolor={fkey: i_to_rgb(dev2[fkey]) for fkey in mesh2.faces()})
-plotter.show()
+#plotter = MeshPlotter(mesh2)
+#plotter.draw_faces(facecolor={fkey: i_to_rgb(dev2[fkey]) for fkey in mesh2.faces()})
+#plotter.show()
