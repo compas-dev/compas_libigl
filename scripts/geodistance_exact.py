136f6847db9488465325b94647c72d968d53ccdf
Tom Van Mele <vanmelet@ethz.ch>
2019-11-13 16:13:10 +0100

    updated to latest igl and pybind added geodistance (heat and exact)

---
+++ scripts/geodistance_exact.py
@@ -0,0 +1,32 @@
+import numpy
+import compas
+import os
+
+from compas.datastructures import Mesh
+from compas.datastructures import mesh_quads_to_triangles
+from compas_plotters import MeshPlotter
+
+import compas_libigl as igl
+
+# mesh = Mesh.from_off('../../../data/libigl-tutorial-data/bunny.off')
+# mesh = Mesh.from_obj('../../../data/libigl-tutorial-data/armadillo.obj')
+
+HERE = os.path.dirname(__file__)
+FILE = os.path.join(HERE, '..', 'data', 'tubemesh.json')
+
+mesh = Mesh.from_json(FILE)
+mesh_quads_to_triangles(mesh)
+
+key_index = mesh.key_index()
+
+vertices = mesh.get_vertices_attributes('xyz')
+faces = [[key_index[key] for key in mesh.face_vertices(fkey)] for fkey in mesh.faces()]
+
+V = numpy.array(vertices, dtype=numpy.float64)
+F = numpy.array(faces, dtype=numpy.int32)
+d = igl.trimesh_geodistance_exact(V, F, 0)
+
+plotter = MeshPlotter(mesh, figsize=(10, 7))
+plotter.draw_faces()
+plotter.draw_vertices(text={key: "{:.0f}".format(d[key_index[key]]) for key in mesh.vertices()})
+plotter.show()
--- scripts/geodistance_heat.py
+++ scripts/geodistance_heat.py
@@ -1,31 +1,32 @@
 import numpy
 import compas
 import os
+
 from compas.datastructures import Mesh
 from compas.datastructures import mesh_quads_to_triangles
-
 from compas_plotters import MeshPlotter
 
-import geodistance as g
+import compas_libigl as igl
 
 # mesh = Mesh.from_off('../../../data/libigl-tutorial-data/bunny.off')
 # mesh = Mesh.from_obj('../../../data/libigl-tutorial-data/armadillo.obj')
-dir_path = os.path.dirname(os.path.realpath(__file__))
-mesh = Mesh.from_json(dir_path + '/tubemesh.json')
 
+HERE = os.path.dirname(__file__)
+FILE = os.path.join(HERE, '..', 'data', 'tubemesh.json')
+
+mesh = Mesh.from_json(FILE)
 mesh_quads_to_triangles(mesh)
-
 
 key_index = mesh.key_index()
 
-V = numpy.array(mesh.get_vertices_attributes('xyz'), dtype=numpy.float64)
-F = numpy.array([[key_index[key] for key in mesh.face_vertices(fkey)] for fkey in mesh.faces()], dtype=numpy.int32)
+vertices = mesh.get_vertices_attributes('xyz')
+faces = [[key_index[key] for key in mesh.face_vertices(fkey)] for fkey in mesh.faces()]
 
-d = g.exact(V, F, 0)
+V = numpy.array(vertices, dtype=numpy.float64)
+F = numpy.array(faces, dtype=numpy.int32)
+d = igl.trimesh_geodistance_heat(V, F, 0)
 
-print(d)
-
-plotter = MeshPlotter(mesh)
+plotter = MeshPlotter(mesh, figsize=(10, 7))
 plotter.draw_faces()
 plotter.draw_vertices(text={key: "{:.0f}".format(d[key_index[key]]) for key in mesh.vertices()})
 plotter.show()
