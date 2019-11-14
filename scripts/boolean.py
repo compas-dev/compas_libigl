323e9c529545edc97c7bd665684be46aa01551d3
Tom Van Mele <vanmelet@ethz.ch>
2019-11-14 16:20:25 +0100

    boolean with viewer but no viewer instructions yet

--- scripts/boolean.py
+++ scripts/boolean.py
@@ -1,21 +1,23 @@
 import numpy
+
 import compas
 from compas.geometry import Box
 from compas.datastructures import Mesh
 from compas.datastructures import mesh_quads_to_triangles
 from compas.datastructures import mesh_subdivide_quad
-from compas_plotters import MeshPlotter
+
+from compas_viewers import MeshViewer
 
 import compas_libigl as igl
 
 box = Box.from_width_height_depth(5.0, 3.0, 1.0)
 a = Mesh.from_vertices_and_faces(box.vertices, box.faces)
-a = mesh_subdivide_quad(a, k=3)
+a = mesh_subdivide_quad(a, k=2)
 mesh_quads_to_triangles(a)
 
 box = Box.from_width_height_depth(1.0, 5.0, 3.0)
 b = Mesh.from_vertices_and_faces(box.vertices, box.faces)
-b = mesh_subdivide_quad(b, k=3)
+b = mesh_subdivide_quad(b, k=2)
 mesh_quads_to_triangles(b)
 
 VA = numpy.array(a.get_vertices_attributes('xyz'), dtype=numpy.float64)
@@ -28,7 +30,6 @@ result = igl.mesh_union(VA, FA, VB, FB)
 
 c = Mesh.from_vertices_and_faces(result.vertices, result.faces)
 
-plotter= MeshPlotter(c, figsize=(10, 7))
-plotter.draw_vertices(radius=0.01)
-plotter.draw_edges()
-plotter.show()
+viewer = MeshViewer()
+viewer.mesh = c
+viewer.show()
