5adfba870b60ca06456de35608401e1795bfe5e7
brgcode <brg@arch.ethz.ch>
2019-12-07 23:27:42 +0100

    added "in place" alternative for planarize proxy

--- scripts/planarize_quads.py
+++ scripts/planarize_quads.py
@@ -7,13 +7,19 @@ from compas_plotters import MeshPlotter
 from compas.utilities import i_to_rgb
 from compas_libigl import planarize_quads
 
-HERE = os.path.dirname(__file__)
-FILE = os.path.join(HERE, '..', 'data', 'tubemesh.json')
-
 MAXDEV = 0.005
 KMAX = 500
 
+HERE = os.path.dirname(__file__)
+
+FILE = os.path.join(HERE, '..', 'data', 'tubemesh.json')
 mesh1 = Mesh.from_json(FILE)
+
+# FILE = os.path.join(HERE, '../data/libigl-tutorial-data/inspired_mesh_quads_Conjugate.off')
+# mesh1 = Mesh.from_off(FILE)
+
+# print(mesh1.number_of_vertices())
+# print(mesh1.number_of_faces())
 
 vertices, faces = mesh1.to_vertices_and_faces()
 
@@ -22,7 +28,7 @@ F1 = numpy.array(faces, dtype=numpy.int32)
 
 V2 = planarize_quads(V1, F1, KMAX, MAXDEV)
 
-mesh2 = Mesh.from_vertices_and_faces(V2, faces)
+mesh2 = Mesh.from_vertices_and_faces(V2, F1)
 dev2 = mesh_flatness(mesh2, maxdev=MAXDEV)
 
 plotter = MeshPlotter(mesh2, figsize=(8, 5))
