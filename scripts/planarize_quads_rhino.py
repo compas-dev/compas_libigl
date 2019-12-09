6c621afa34126a7afd27c37af17c53d53a4c530a
Tom Van Mele <vanmelet@ethz.ch>
2019-12-09 09:29:21 +0100

    mac build

--- scripts/planarize_quads_rhino.py
+++ scripts/planarize_quads_rhino.py
@@ -9,11 +9,13 @@ from compas_rhino.artists import MeshArtist
 
 from compas.rpc import Proxy
 
-igl = Proxy('compas_libigl')
-# igl.stop_server()
-# igl.start_server()
+proxy = Proxy()
 
-planaryze_quads = igl.planarize_quads_proxy
+proxy.package = 'compas_libigl'
+# proxy.stop_server()
+# proxy.start_server()
+
+planarize_quads = proxy.planarize_quads_proxy
 
 HERE = os.path.dirname(__file__)
 FILE = os.path.join(HERE, '..', 'data', 'tubemesh.json')
@@ -21,7 +23,7 @@ FILE = os.path.join(HERE, '..', 'data', 'tubemesh.json')
 mesh1 = Mesh.from_json(FILE)
 
 V1, F1 = mesh1.to_vertices_and_faces()
-V2 = planaryze_quads(V1, F1, 500, 0.005)
+V2 = planarize_quads(V1, F1, 500, 0.005)
 
 mesh2 = Mesh.from_vertices_and_faces(V2, F1)
 dev2 = mesh_flatness(mesh2, maxdev=0.005)
