525ea3176f5226673dbe0859f6f8e4d904a96ccc
Tom Van Mele <vanmelet@ethz.ch>
2020-03-28 09:32:19 +0100

    updated readme simplified planarize quads example with tube off

--- scripts/planarize_quads.py
+++ scripts/planarize_quads.py
@@ -1,4 +1,3 @@
-import numpy
 import os
 import compas
 from compas.datastructures import Mesh
@@ -7,30 +6,24 @@ from compas_plotters import MeshPlotter
 from compas.utilities import i_to_rgb
 from compas_libigl import planarize_quads
 
+TOL = 0.01
 MAXDEV = 0.005
 KMAX = 500
 HERE = os.path.dirname(__file__)
 
-# note: other planarization options
-
 # FILE = os.path.join(HERE, '..', 'data', 'tubemesh.json')
 # mesh1 = Mesh.from_json(FILE)
 
-FILE = os.path.join(HERE, '../data/libigl-tutorial-data/inspired_mesh_quads_Conjugate.off')
-mesh1 = Mesh.from_off(FILE)
+# FILE = os.path.join(HERE, '../data/libigl-tutorial-data/inspired_mesh_quads_Conjugate.off')
+# FILE = os.path.join(HERE, '../data/bunny.off')
 
-print(mesh1.number_of_vertices())
-print(mesh1.number_of_faces())
+mesh1 = Mesh.from_off(compas.get('tubemesh.off'))
 
 vertices, faces = mesh1.to_vertices_and_faces()
+vertices = planarize_quads(vertices, faces, KMAX, MAXDEV)
 
-V1 = numpy.array(vertices, dtype=numpy.float64)
-F1 = numpy.array(faces, dtype=numpy.int32)
-
-V2 = planarize_quads(V1, F1, KMAX, MAXDEV)
-
-mesh2 = Mesh.from_vertices_and_faces(V2, F1)
-dev2 = mesh_flatness(mesh2, maxdev=MAXDEV)
+mesh2 = Mesh.from_vertices_and_faces(vertices, faces)
+dev2 = mesh_flatness(mesh2, maxdev=TOL)
 
 plotter = MeshPlotter(mesh2, figsize=(8, 5))
 plotter.draw_faces(facecolor={fkey: i_to_rgb(dev2[fkey]) for fkey in mesh2.faces()})
