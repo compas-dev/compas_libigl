19f81e37f72c95cab88d54962ab81961a078caa8
Tom Van Mele <vanmelet@ethz.ch>
2020-05-02 11:43:02 +0200

    removing booleans until salilab stuff has been added to conda forge

--- src/compas_libigl/__init__.py
+++ src/compas_libigl/__init__.py
@@ -2,7 +2,7 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
-from .booleans import *
+# from .booleans import *
 from .geodistance import *
 from .isolines import *
 from .planarize import *
