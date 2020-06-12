f9a6237176d55c5977ea32cf9c77f3e250fc7e73
Tom Van Mele <vanmelet@ethz.ch>
2020-06-12 15:16:06 +0200

    isolines helper function

--- examples/isolines.py
+++ examples/isolines.py
@@ -1,8 +1,7 @@
 import math
-from itertools import groupby
 import compas_libigl as igl
 from compas.datastructures import Mesh
-from compas.utilities import Colormap
+from compas.utilities import Colormap, rgb_to_hex
 from compas.geometry import Line, Polyline, Rotation, Scale
 from compas_viewers.objectviewer import ObjectViewer
 
@@ -24,62 +23,25 @@ mesh.transform(S * Rz * Rx)
 
 scalars = mesh.vertices_attribute('z')
 vertices, edges = igl.trimesh_isolines(mesh, scalars, 10)
-levels = groupby(sorted(edges, key=lambda edge: vertices[edge[0]][2]), key=lambda edge: round(vertices[edge[0]][2], 3))
-
-isolines = []
-for value, edges in levels:
-    paths = []
-    edges = [edge.tolist() for edge in edges]
-    edge = edges.pop()
-    paths.append([edge])
-    while edges:
-        for edge in edges:
-            found = False
-            for path in paths:
-                a = path[0][0]
-                z = path[-1][1]
-                u, v = edge
-                if u == z:
-                    path.append([u, v])
-                    edges.remove(edge)
-                    found = True
-                    break
-                if v == z:
-                    path.append([v, u])
-                    edges.remove(edge)
-                    found = True
-                    break
-                if v == a:
-                    path.insert(0, [u, v])
-                    edges.remove(edge)
-                    found = True
-                    break
-                if u == a:
-                    path.insert(0, [v, u])
-                    edges.remove(edge)
-                    found = True
-                    break
-            if not found:
-                paths.append([edge])
-                edges.remove(edge)
-    isolines.append((value, paths))
+isolines = igl.groupsort_isolines(vertices, edges)
 
 # ==============================================================================
 # Visualisation
 # ==============================================================================
 
-cmap = Colormap(scalars, 'rgb')
-
 viewer = ObjectViewer()
 
-viewer.add(mesh, settings={'color': '#00ff00', 'vertices.on': False, 'edges.on': False})
+viewer.add(mesh, settings={'color': '#ffffff', 'vertices.on': False, 'edges.on': False})
 
+cmap = Colormap(scalars, 'rgb')
 for value, paths in isolines:
     for path in paths:
         points = [vertices[path[0][0]]]
         for i, j in path:
             points.append(vertices[j])
-        viewer.add(Polyline(points), settings={'edges.color': '#ff0000', 'edges.width': 5, 'vertices.on': False})
+        viewer.add(
+            Polyline(points),
+            settings={'edges.color': rgb_to_hex(cmap(value)), 'edges.width': 5, 'vertices.on': False})
 
 viewer.update()
 viewer.show()
