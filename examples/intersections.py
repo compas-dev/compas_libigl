2f9f95d49da54406f808777bef84a54ce826c141
Tom Van Mele <vanmelet@ethz.ch>
2020-06-13 09:49:38 +0200

    with reflections

--- examples/intersections.py
+++ examples/intersections.py
@@ -2,12 +2,26 @@ import math
 import numpy as np
 import compas_libigl as igl
 
-from compas.geometry import add_vectors, scale_vector
+from compas.geometry import add_vectors, scale_vector, cross_vectors
+from compas.geometry import intersection_line_plane
 from compas.geometry import Point, Vector, Line, Plane, Sphere
 from compas.geometry import Reflection, Translation
 from compas.datastructures import Mesh
 
 from compas_viewers.objectviewer import ObjectViewer
+
+
+def ray_mesh_intersect_reflect(ray, mesh, hit, facemap):
+    base, vector = ray
+    index = hit[0]
+    distance = hit[3]
+    face = facemap[index]
+    p = base + vector * distance
+    n = Vector(* mesh.face_normal(face))
+    n = n.cross(vector.cross(n))
+    r = base.transformed(Reflection.from_plane((p, n)))
+    return p, r
+
 
 # ==============================================================================
 # Input geometry
@@ -30,8 +44,8 @@ base = Point(-7, 0, 0)
 
 sphere = Sphere(base, 1.0)
 
-theta = np.linspace(0, np.pi, 20, endpoint=False)
-phi = np.linspace(0, 2 * np.pi, 20, endpoint=False)
+theta = np.linspace(0, np.pi, 10, endpoint=False)
+phi = np.linspace(0, 2 * np.pi, 10, endpoint=False)
 theta, phi = np.meshgrid(theta, phi)
 theta = theta.ravel()
 phi = phi.ravel()
@@ -56,25 +70,37 @@ for x, y, z in hemi:
 # ==============================================================================
 
 intersections = []
-# reflections = []
+reflections = []
 
 ground = Plane([0, 0, 0], [0, 0, 1])
 
-faces = list(mesh.faces())
+index_face = {index: face for index, face in enumerate(mesh.faces())}
 
 for ray in rays:
-    base, vector = ray
     hits = igl.intersection_ray_mesh(ray, mesh)
     if hits:
-        i = hits[0][0]
-        d = hits[0][3]
-        if i in faces:
-            n = scale_vector(mesh.face_normal(i), -1)
-            x = add_vectors(base, scale_vector(vector, d))
-            p = Point(*x)
-            # r = base.transformed(Reflection.from_plane((p, n)))
-            intersections.append(p)
-            # reflections.append(r)
+        i1, r1 = ray_mesh_intersect_reflect(ray, mesh, hits[0], index_face)
+        intersections.append(i1)
+        vector = r1 - i1
+        vector.unitize()
+        ray = i1, vector
+        hits = igl.intersection_ray_mesh(ray, mesh)
+        if hits:
+            if len(hits) > 1:
+                hit = hits[1]
+            else:
+                hit = hits[0]
+            i2, _ = ray_mesh_intersect_reflect(ray, mesh, hit, index_face)
+            if (i2 - i1).length < 0.01:
+                x = intersection_line_plane((i1, r1), ground)
+                r1 = Point(* x)
+                reflections.append((i1, r1))
+            else:
+                reflections.append((i1, i2))
+        else:
+            x = intersection_line_plane((i1, r1), ground)
+            r1 = Point(* x)
+            reflections.append((i1, r1))
 
 # ==============================================================================
 # Visualisation
@@ -82,11 +108,13 @@ for ray in rays:
 
 viewer = ObjectViewer()
 
-viewer.add(mesh, settings={'color': '#cccccc', 'opacity': 0.7, 'vertices.on': False})
+viewer.add(mesh, settings={'color': '#cccccc', 'opacity': 0.5, 'edges.on': False})
 
-for p in intersections:
-    viewer.add(Line(base, p), settings={'edges.color': '#ff0000', 'edges.width': 2})
-    # viewer.add(Line(p, r), settings={'edges.color': '#0000ff', 'edges.width': 2})
+for intersection in intersections:
+    viewer.add(Line(base, intersection), settings={'edges.color': '#ff0000', 'edges.width': 3})
+
+for reflection in reflections:
+    viewer.add(Line(* reflection), settings={'edges.color': '#0000ff', 'edges.width': 1})
 
 viewer.update()
 viewer.show()
