41195944369722dadce62e13e65f2a9e4313f15c
tomvanmele <tom.v.mele@gmail.com>
2023-11-22 13:23:39 +0100

    Bump version to 0.2.0

--- setup.py
+++ setup.py
@@ -96,7 +96,7 @@ optional_requirements = {}
 
 setup(
     name="compas_libigl",
-    version="0.1.0",
+    version="0.2.0",
     description="Opinionated COMPAS compatible bindings for top-level algorithms of libigl.",
     long_description=long_description,
     long_description_content_type="text/markdown",
