a7b56c1b62879a07a6fef57230ce4cd700e99972
tomvanmele <tom.v.mele@gmail.com>
2023-11-22 13:10:31 +0100

    prep for release

--- recipe/sha256.py
+++ recipe/sha256.py
@@ -1,13 +1,9 @@
-import os
 import hashlib
+from pathlib import Path
 
-# HERE = os.path.dirname(__file__)
-# FILE = os.path.join(HERE, '/Users/vanmelet/Downloads/pybind11-2.5.0.tar.gz')
-# FILE = '/Users/vanmelet/Downloads/pybind11-2.5.0.tar.gz'
-# FILE = '/Users/vanmelet/Downloads/libigl-2.2.0.tar.gz'
-FILE = '/Users/vanmelet/Downloads/compas_libigl-0.1.9.tar.gz'
+FILE = Path(__file__).parent / "pybind11-2.5.0.tar.gz"
 
-with open(FILE, 'rb') as f:
+with open(FILE, "rb") as f:
     data = f.read()
     h = hashlib.sha256(data).hexdigest()
 
