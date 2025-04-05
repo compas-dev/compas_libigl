08007a5a1e5e34890af04c325ac9ab978a58f30d
tomvanmele <tom.v.mele@gmail.com>
2025-04-05 10:06:42 +0200

    Bump version to 0.4.0

--- src/compas_libigl/__init__.py
+++ src/compas_libigl/__init__.py
@@ -16,7 +16,7 @@ __author__ = ["tom van mele", "petras vestartas"]
 __copyright__ = "Block Research Group - ETH Zurich"
 __license__ = "Mozilla Public License Version 2.0"
 __email__ = "van.mele@arch.ethz.ch, petrasvestartas@gmail.com"
-__version__ = "0.3.1"
+__version__ = "0.4.0"
 
 
 HERE = os.path.dirname(__file__)
