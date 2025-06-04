a9d9afc16d619a0c6eb4b9adfdd7a3ed361e08f3
pv <petasvestartas@gmail.com>
2025-06-04 13:28:43 +0200

    ADD map_pattern_to_mesh method

--- docs/examples/example_mapping_all_patterns.py
+++ docs/examples/example_mapping_all_patterns.py
@@ -1,57 +1,11 @@
-from itertools import filterfalse
 from pathlib import Path
 
 from compas.colors import Color
 from compas.datastructures import Mesh
 from compas_viewer import Viewer
 from compas_viewer.config import Config
-from tessagon.adaptors.list_adaptor import ListAdaptor
-from tessagon.types.brick_tessagon import BrickTessagon
-from tessagon.types.dissected_hex_quad_tessagon import DissectedHexQuadTessagon
-from tessagon.types.dissected_hex_tri_tessagon import DissectedHexTriTessagon
-from tessagon.types.dissected_square_tessagon import DissectedSquareTessagon
-from tessagon.types.dissected_triangle_tessagon import DissectedTriangleTessagon
-from tessagon.types.dodeca_tessagon import DodecaTessagon
-from tessagon.types.floret_tessagon import FloretTessagon
-from tessagon.types.hex_big_tri_tessagon import HexBigTriTessagon
-from tessagon.types.hex_tessagon import HexTessagon
-from tessagon.types.hex_tri_tessagon import HexTriTessagon
-from tessagon.types.octo_tessagon import OctoTessagon
-from tessagon.types.pythagorean_tessagon import PythagoreanTessagon
-from tessagon.types.rhombus_tessagon import RhombusTessagon
-from tessagon.types.square_tessagon import SquareTessagon
-from tessagon.types.square_tri_tessagon import SquareTriTessagon
-from tessagon.types.tri_tessagon import TriTessagon
-from tessagon.types.weave_tessagon import WeaveTessagon
-from tessagon.types.zig_zag_tessagon import ZigZagTessagon
 
-from compas_libigl.mapping import map_mesh
-from compas_libigl.parametrisation import trimesh_lsc_mapping
-
-TESSAGON_TYPES = {
-    1: ("Hex", HexTessagon),
-    2: ("Tri", TriTessagon),
-    3: ("Octo", OctoTessagon),
-    4: ("Square", SquareTessagon),
-    5: ("Rhombus", RhombusTessagon),
-    6: ("HexTri", HexTriTessagon),
-    7: ("DissectedSquare", DissectedSquareTessagon),
-    8: ("DissectedTriangle", DissectedTriangleTessagon),
-    9: ("DissectedHexQuad", DissectedHexQuadTessagon),
-    10: ("DissectedHexTri", DissectedHexTriTessagon),
-    11: ("Floret", FloretTessagon),
-    12: ("Pythagorean", PythagoreanTessagon),
-    13: ("Brick", BrickTessagon),
-    14: ("Weave", WeaveTessagon),
-    15: ("ZigZag", ZigZagTessagon),
-    16: ("HexBigTri", HexBigTriTessagon),
-    17: ("Dodeca", DodecaTessagon),
-    18: ("SquareTri", SquareTriTessagon),
-}
-
-# Pattern selection - change this value to use a different pattern
-
-PATTERN_TYPE = 15
+from compas_libigl.mapping import map_pattern_to_mesh
 
 # ==============================================================================
 # Input geometry: 3D Mesh
@@ -63,62 +17,23 @@ for vertex in mesh.vertices():
     x, y, z = mesh.vertex_attributes(vertex, "xyz")  # type: ignore
     mesh.vertex_attributes(vertex, "xyz", [x, -z, y])
 
-mesh.translate([2, 2, 0.5])
-
-v, f = mesh.to_vertices_and_faces()
-
-# ==============================================================================
-# Input geometry: 2D Pattern creation using Tessagon library, can be other mesh.
-# ==============================================================================
-
-options = {
-    "function": lambda u, v: [u * 1, v * 1, 0],
-    "u_range": [-0.25, 1.25],
-    "v_range": [-0.25, 1.25],
-    "u_num": 20,
-    "v_num": 20,
-    "u_cyclic": False,
-    "v_cyclic": False,
-    "adaptor_class": ListAdaptor,
-}
-
-# Get pattern name and class based on selected pattern type
-pattern_name, pattern_class = TESSAGON_TYPES[PATTERN_TYPE]
-
-# Create the selected tessagon pattern
-tessagon = pattern_class(**options)
-tessagon_mesh = tessagon.create_mesh()
-pv = tessagon_mesh["vert_list"]
-pf = tessagon_mesh["face_list"]
-
 # ==============================================================================
 # Mapping: 3D Mesh, 2D Pattern, UV
 # ==============================================================================
-
-mv, mf = map_mesh((v, f), (pv, pf), clip_boundaries=True, tolerance=1e-6)
-mesh_mapped = Mesh.from_vertices_and_faces(mv, mf)
-
-print(len(mv))
+mesh_mapped = map_pattern_to_mesh("ZigZag", mesh, clip_boundaries=True, tolerance=1e-6, pattern_u=10, pattern_v=10)
 
 # ==============================================================================
 # Viewer
 # ==============================================================================
 
 config = Config()
-config.camera.target = [2, 2, 0.25]
-config.camera.position = [5, 2, 1.5]
+config.camera.target = [0, 0, 0]
+config.camera.position = [0.75, 0, 0.75]
 
 viewer = Viewer(config=config)
 
 viewer.scene.add(mesh, name="mesh", show_faces=False, linecolor=Color.grey(), opacity=0.2)
-viewer.scene.add(Mesh.from_vertices_and_faces(pv, pf), name="pattern2d")
 viewer.scene.add(mesh_mapped, name="mesh_mapped", facecolor=Color.red())
 
-# To see where the pattern is mapped:
-uv = trimesh_lsc_mapping((v, f))
-mesh_flattened = mesh.copy()
-for i in range(mesh.number_of_vertices()):
-    mesh_flattened.vertex_attributes(i, "xyz", [uv[i][0], uv[i][1], 0])
-viewer.scene.add(mesh_flattened, name="mesh_flattened", show_lines=False, opacity=0.5)
 
 viewer.show()
--- src/compas_libigl/mapping.py
+++ src/compas_libigl/mapping.py
@@ -1,6 +1,27 @@
 import numpy as np
+from compas.datastructures import Mesh
+from tessagon.adaptors.list_adaptor import ListAdaptor
+from tessagon.types.brick_tessagon import BrickTessagon
+from tessagon.types.dissected_hex_quad_tessagon import DissectedHexQuadTessagon
+from tessagon.types.dissected_hex_tri_tessagon import DissectedHexTriTessagon
+from tessagon.types.dissected_square_tessagon import DissectedSquareTessagon
+from tessagon.types.dissected_triangle_tessagon import DissectedTriangleTessagon
+from tessagon.types.dodeca_tessagon import DodecaTessagon
+from tessagon.types.floret_tessagon import FloretTessagon
+from tessagon.types.hex_big_tri_tessagon import HexBigTriTessagon
+from tessagon.types.hex_tessagon import HexTessagon
+from tessagon.types.hex_tri_tessagon import HexTriTessagon
+from tessagon.types.octo_tessagon import OctoTessagon
+from tessagon.types.pythagorean_tessagon import PythagoreanTessagon
+from tessagon.types.rhombus_tessagon import RhombusTessagon
+from tessagon.types.square_tessagon import SquareTessagon
+from tessagon.types.square_tri_tessagon import SquareTriTessagon
+from tessagon.types.tri_tessagon import TriTessagon
+from tessagon.types.weave_tessagon import WeaveTessagon
+from tessagon.types.zig_zag_tessagon import ZigZagTessagon
 
 from compas_libigl import _mapping
+from compas_libigl._types_std import VectorVectorInt  # noqa: F401
 
 
 def map_mesh(target_mesh, pattern_mesh, clip_boundaries=True, tolerance=1e-6):
@@ -45,3 +66,91 @@ def map_mesh(target_mesh, pattern_mesh, clip_boundaries=True, tolerance=1e-6):
 
     # Return the result as a tuple
     return pattern_v_numpy_copy, pattern_f_numpy_cleaned
+
+
+def map_pattern_to_mesh(name, mesh, clip_boundaries=True, tolerance=1e-6, pattern_u=16, pattern_v=16):
+    """
+    Map a 2D pattern mesh onto a 3D target.
+
+    Parameters
+    ----------
+    name : str
+        The name of the pattern to be created. Options are:
+        "Hex",
+        "Tri",
+        "Octo",
+        "Square",
+        "Rhombus",
+        "HexTri",
+        "DissectedSquare",
+        "DissectedTriangle",
+        "DissectedHexQuad",
+        "DissectedHexTri",
+        "Floret",
+        "Pythagorean",
+        "Brick",
+        "Weave",
+        "ZigZag",
+        "HexBigTri",
+        "Dodeca",
+        "SquareTri"
+    mesh : compas.datastructures.Mesh
+        The target mesh.
+    clip_boundaries : bool
+        Whether to clip the pattern mesh to the boundaries of the target mesh.
+    tolerance : float
+        The tolerance for point comparison, to remove duplicates.
+    pattern_u : int
+        The number of pattern vertices in the u direction.
+    pattern_v : int
+        The number of pattern vertices in the v direction.
+
+    Returns
+    -------
+    compas.datastructures.Mesh
+        The mapped pattern mesh.
+    """
+
+    TESSAGON_TYPES = {
+        "Hex": HexTessagon,
+        "Tri": TriTessagon,
+        "Octo": OctoTessagon,
+        "Square": SquareTessagon,
+        "Rhombus": RhombusTessagon,
+        "HexTri": HexTriTessagon,
+        "DissectedSquare": DissectedSquareTessagon,
+        "DissectedTriangle": DissectedTriangleTessagon,
+        "DissectedHexQuad": DissectedHexQuadTessagon,
+        "DissectedHexTri": DissectedHexTriTessagon,
+        "Floret": FloretTessagon,
+        "Pythagorean": PythagoreanTessagon,
+        "Brick": BrickTessagon,
+        "Weave": WeaveTessagon,
+        "ZigZag": ZigZagTessagon,
+        "HexBigTri": HexBigTriTessagon,
+        "Dodeca": DodecaTessagon,
+        "SquareTri": SquareTriTessagon,
+    }
+
+    options = {
+        "function": lambda u, v: [u * 1, v * 1, 0],
+        "u_range": [-0.255, 1.33],
+        "v_range": [-0.34, 1.33],
+        "u_num": pattern_u,
+        "v_num": pattern_v,
+        "u_cyclic": False,
+        "v_cyclic": False,
+        "adaptor_class": ListAdaptor,
+    }
+
+    # Create the selected tessagon pattern
+    pattern_class = TESSAGON_TYPES[name]
+    tessagon = pattern_class(**options)
+    tessagon_mesh = tessagon.create_mesh()
+    pv = tessagon_mesh["vert_list"]
+    pf = tessagon_mesh["face_list"]
+
+    v, f = mesh.to_vertices_and_faces()
+    mv, mf = map_mesh((v, f), (pv, pf), clip_boundaries=clip_boundaries, tolerance=tolerance)
+
+    return Mesh.from_vertices_and_faces(mv, mf)
