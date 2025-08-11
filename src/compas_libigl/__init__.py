915610df236ba5eb9c6786af203cdff748c1b4d4
Chen Kasirer <ckasirer@ethz.ch>
2025-08-11 13:16:24 +0200

    added __all_plugins__ to root package

--- src/compas_libigl/__init__.py
+++ src/compas_libigl/__init__.py
@@ -3,3 +3,14 @@ __copyright__ = "Block Research Group - ETH Zurich"
 __license__ = "Mozilla Public License Version 2.0"
 __email__ = "van.mele@arch.ethz.ch, petrasvestartas@gmail.com"
 __version__ = "0.7.4"
+
+
+__all_plugins__ = [
+    "compas_libigl.geodistance",
+    "compas_libigl.intersections",
+    "compas_libigl.isolines",
+    "compas_libigl.massmatrix",
+    "compas_libigl.parametrisation",
+    "compas_libigl.planarize",
+    "compas_libigl.meshing",
+]
